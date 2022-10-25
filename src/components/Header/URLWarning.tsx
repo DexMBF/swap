1f4f89328310ee6e238d26d49f066bddac588552
DexMBF <109440112+DexMBF@users.noreply.github.com>
2022-10-25 15:25:11 +0200

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,7 +30,7 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={11} /> Make sure the URL is
-        <p style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://swap.metabook.finance</p>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://swap.metabook.finance</code>
       </div>
       <StyledClose size={11} onClick={toggleURLWarning} />
     </PhishAlert>
@@ -38,7 +38,7 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={11} /> Always make sure the URL is
-        <p style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://swap.metabook.finance/</p> -
+        <code style={{ padding: '0 4px', display: 'inline', line-height: '1.5', fontWeight: 'bold' }}>https://swap.metabook.finance/</code> -
         bookmark it to be safe.
       </div>
       <StyledClose size={11} onClick={toggleURLWarning} />
